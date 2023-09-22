415208fc89c48178dc84dcd3ba2781ec246067d7
mialskywalker <kalqga123@gmail.com>
2023-09-22 11:25:40 +0300

    deposit calc

---
+++ depositCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace depositCalculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double gdp = double.Parse(Console.ReadLine());
+
+            double result = deposit + ((deposit * gdp / 100) / 12) * term;
+
+            Console.WriteLine(result);
+        }
+    }
+}
