714febea55fea5ccaf4c0d0172c74cc918dd5695
mialskywalker <kalqga123@gmail.com>
2023-09-22 11:33:21 +0300

    fish tank

---
+++ fishTank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fishTank
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            int aqVolume = length * width * height;
+            double literVolume = aqVolume * 0.001;
+            double needed = literVolume * (1 - percentage / 100);
+
+            Console.WriteLine(needed);
+        }
+    }
+}
