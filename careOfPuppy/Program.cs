abf17320a2b61fe5f58835928876d64ebc9d165c
mialskywalker <kalqga123@gmail.com>
2023-09-22 13:31:55 +0300

    care of puppy

---
+++ careOfPuppy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace careOfPuppy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int boughtFoodinKG = int.Parse(Console.ReadLine());
+            double boughtFood = boughtFoodinKG * 1000;
+            string command = "";
+            double eaten = 0.0;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "Adopted")
+                {
+                    break;
+                }
+                else
+                {
+                    eaten += int.Parse(command);
+                }
+            }
+
+            double difference = boughtFood - eaten;
+
+            if (difference >= 0)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {difference} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {Math.Abs(difference)} grams more.");
+            }
+        }
+    }
+}
