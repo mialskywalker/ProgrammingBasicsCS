738c0ab2609e268004d35cd3d0c82f3baa2d00f8
mialskywalker <kalqga123@gmail.com>
2023-09-22 12:05:40 +0300

    Energy Booster

---
+++ catWalking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace catWalking
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minutesPerDay = int.Parse(Console.ReadLine());
+            int numberOfWalksPerDay = int.Parse(Console.ReadLine());
+            int caloriesTaken = int.Parse(Console.ReadLine());
+
+            int totalWalkMins = numberOfWalksPerDay * minutesPerDay;
+            int totalCaloriesBurned = totalWalkMins * 5;
+
+            double caloriesNeeded = caloriesTaken * 0.5;
+
+            if (caloriesNeeded <= totalCaloriesBurned)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {totalCaloriesBurned}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {totalCaloriesBurned}.");
+            }
+        }
+    }
+}
