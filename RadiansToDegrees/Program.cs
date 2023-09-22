809a26b4a75dcbe617fd113bb1b62146511adc75
mialskywalker <kalqga123@gmail.com>
2023-09-22 11:13:09 +0300

    deposit calculator

--- RadiansToDegrees/Program.cs
+++ RadiansToDegrees/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace RadiansToDegrees
+﻿using System;
+
+namespace RadiansToDegrees
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            double rad = double.Parse(Console.ReadLine());
+            double degrees = rad * 180 / Math.PI;
+
+            Console.WriteLine(degrees);
+
         }
     }
 }
