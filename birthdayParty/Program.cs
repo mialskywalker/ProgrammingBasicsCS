f0db11bda2b73e7f1f23482239ad04108b2ec644
mialskywalker <kalqga123@gmail.com>
2023-09-22 11:58:58 +0300

    birthday party

---
+++ birthdayParty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace birthdayParty
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double roomRent = double.Parse(Console.ReadLine());
+
+            double cakePrice = roomRent * 0.2;
+            double drinksPrice = cakePrice - (cakePrice * 0.45);
+            double animator = roomRent / 3;
+            double needed = roomRent + cakePrice + drinksPrice + animator;
+
+            Console.WriteLine(needed);
+        }
+    }
+}
