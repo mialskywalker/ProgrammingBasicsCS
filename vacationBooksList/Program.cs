22440788722b0f720d15d6bc077b2f3c8cf65909
mialskywalker <kalqga123@gmail.com>
2023-09-22 11:29:21 +0300

    vacation books list

---
+++ vacationBooksList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace vacationBooksList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int totalPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int hours = (totalPages / pagesPerHour) / days;
+            Console.WriteLine(hours);
+        }
+    }
+}
